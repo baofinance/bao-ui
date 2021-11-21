cc1a5e98b270d87a419dee519ed16c07ee917133
Ben Clabby <benclabby@gmail.com>
2021-11-21 16:55:03 -0500

    Exchange Rate & Anchor Prices hooks

--- src/hooks/hard-synths/useExchangeRates.ts
+++ src/hooks/hard-synths/useExchangeRates.ts
@@ -1,3 +1,52 @@
-export const useExchangeRates = () => {
+import { useCallback, useEffect, useState } from 'react'
+import BigNumber from 'bignumber.js'
+import useBao from '../useBao'
+import { SupportedMarket } from '../../bao/lib/types'
+import Config from '../../bao/lib/config'
+import MultiCall from '../../utils/multicall'
 
+type ExchangeRates = {
+  exchangeRates: { [key: string]: BigNumber }
+}
+
+export const useExchangeRates = (): ExchangeRates => {
+  const [exchangeRates, setExchangeRates] = useState<
+    undefined | { [key: string]: BigNumber }
+  >()
+  const bao = useBao()
+
+  const fetchExchangeRates = useCallback(async () => {
+    const tokens = Config.markets.map(
+      (market: SupportedMarket) => market.marketAddresses[Config.networkId],
+    )
+    const multiCallContext = MultiCall.createCallContext(
+      tokens.map((token: string) => ({
+        ref: token,
+        contract: bao.getNewContract('ctoken.json', token),
+        calls: [{ method: 'exchangeRateStored' }],
+      })),
+    )
+    const data = MultiCall.parseCallResults(
+      await bao.multicall.call(multiCallContext),
+    )
+
+    setExchangeRates(
+      Object.keys(data).reduce(
+        (exchangeRate: { [key: string]: BigNumber }, address: any) => ({
+          ...exchangeRate,
+          [address]: new BigNumber(data[address][0].values[0].hex),
+        }),
+        {},
+      ),
+    )
+  }, [bao])
+
+  useEffect(() => {
+    if (!bao) return
+    fetchExchangeRates()
+  }, [bao])
+
+  return {
+    exchangeRates,
+  }
 }
