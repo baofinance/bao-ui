69d5a0fadeaaa04c556de175ce7bfcf3fca26e1a
Ben Clabby <benclabby@gmail.com>
2021-12-02 09:58:05 -0500

    MarketButton hotfix

--- src/views/Markets/components/MarketButton.tsx
+++ src/views/Markets/components/MarketButton.tsx
@@ -51,17 +51,18 @@ export const MarketButton = ({
 		return (
 			<ButtonStack>
 				<SubmitButton disabled={true}>
-					{confirmations
-						? `Transaction Succeeded (${confirmations}/${REQUIRED_CONFIRMATIONS} Confirmations)`
-						: typeof pendingTx === 'string'
-							? (
-									<ExternalLink href={`${Config.defaultRpc.blockExplorerUrls}/tx/${pendingTx}`} target="_blank">
-										Pending Transaction{' '}
-										<FontAwesomeIcon icon='external-link-alt' />
-									</ExternalLink>
-								)
-							: 'Pending Transaction'
-					}
+					{confirmations ? (
+						`Transaction Succeeded (${confirmations}/${REQUIRED_CONFIRMATIONS} Confirmations)`
+					) : typeof pendingTx === 'string' ? (
+						<ExternalLink
+							href={`${Config.defaultRpc.blockExplorerUrls}/tx/${pendingTx}`}
+							target="_blank"
+						>
+							Pending Transaction <FontAwesomeIcon icon="external-link-alt" />
+						</ExternalLink>
+					) : (
+						'Pending Transaction'
+					)}
 				</SubmitButton>
 			</ButtonStack>
 		)
@@ -70,7 +71,9 @@ export const MarketButton = ({
 			case MarketOperations.supply:
 				return (
 					<ButtonStack>
-						{approvals && (asset.underlying === 'ETH' || approvals[asset.underlying].gt(0)) ? (
+						{approvals &&
+						(asset.underlying === 'ETH' ||
+							approvals[asset.underlying].gt(0)) ? (
 							<SubmitButton
 								disabled={isDisabled}
 								onClick={() => {
@@ -78,13 +81,15 @@ export const MarketButton = ({
 									if (asset.underlying === 'ETH')
 										mintTx = marketContract.methods
 											.mint()
-											.send({ from: account, value: val })
+											.send({ from: account, value: val.toString() })
 									else
 										mintTx = marketContract.methods
-											.mint(val)
+											.mint(val.toString())
 											.send({ from: account })
 									mintTx
-										.on('transactionHash', (txHash: string) => setPendingTx(txHash))
+										.on('transactionHash', (txHash: string) =>
+											setPendingTx(txHash),
+										)
 										.on('confirmation', handleConfirmations)
 										.on('error', clearPendingTx)
 									setPendingTx(true)
@@ -101,7 +106,9 @@ export const MarketButton = ({
 										asset.underlying,
 									)
 									approvev2(underlyingContract, marketContract, account)
-										.on('transactionHash', (txHash: string) => setPendingTx(txHash))
+										.on('transactionHash', (txHash: string) =>
+											setPendingTx(txHash),
+										)
 										.on('confirmation', handleConfirmations)
 										.on('error', clearPendingTx)
 									setPendingTx(true)
@@ -120,9 +127,11 @@ export const MarketButton = ({
 							disabled={isDisabled}
 							onClick={() => {
 								marketContract.methods
-									.redeemUnderlying(val)
+									.redeemUnderlying(val.toString())
 									.send({ from: account })
-									.on('transactionHash', (txHash: string) => setPendingTx(txHash))
+									.on('transactionHash', (txHash: string) =>
+										setPendingTx(txHash),
+									)
 									.on('confirmation', handleConfirmations)
 									.on('error', clearPendingTx)
 								setPendingTx(true)
@@ -134,25 +143,29 @@ export const MarketButton = ({
 				)
 
 			case MarketOperations.borrow:
-				return <SubmitButton
-					disabled={isDisabled}
-					onClick={() => {
-						marketContract.methods
-							.borrow(val)
-							.send({ from: account })
-							.on('transactionHash', (txHash: string) => setPendingTx(txHash))
-							.on('confirmation', handleConfirmations)
-							.on('error', clearPendingTx)
-						setPendingTx(true)
-					}}
-				>
-					Borrow
-				</SubmitButton>
+				return (
+					<SubmitButton
+						disabled={isDisabled}
+						onClick={() => {
+							marketContract.methods
+								.borrow(val.toString())
+								.send({ from: account })
+								.on('transactionHash', (txHash: string) => setPendingTx(txHash))
+								.on('confirmation', handleConfirmations)
+								.on('error', clearPendingTx)
+							setPendingTx(true)
+						}}
+					>
+						Borrow
+					</SubmitButton>
+				)
 
 			case MarketOperations.repay:
 				return (
 					<ButtonStack>
-						{approvals && (asset.underlying === 'ETH' || approvals[asset.underlying].gt(0)) ? (
+						{approvals &&
+						(asset.underlying === 'ETH' ||
+							approvals[asset.underlying].gt(0)) ? (
 							<SubmitButton
 								disabled={isDisabled}
 								onClick={() => {
@@ -160,13 +173,15 @@ export const MarketButton = ({
 									if (asset.underlying === 'ETH')
 										mintTx = marketContract.methods
 											.repayBorrow()
-											.send({ from: account, value: val })
+											.send({ from: account, value: val.toString() })
 									else
 										mintTx = marketContract.methods
-											.repayBorrow(val)
+											.repayBorrow(val.toString())
 											.send({ from: account })
 									mintTx
-										.on('transactionHash', (txHash: string) => setPendingTx(txHash))
+										.on('transactionHash', (txHash: string) =>
+											setPendingTx(txHash),
+										)
 										.on('confirmation', handleConfirmations)
 										.on('error', clearPendingTx)
 									setPendingTx(true)
@@ -183,7 +198,9 @@ export const MarketButton = ({
 										asset.underlying,
 									)
 									approvev2(underlyingContract, marketContract, account)
-										.on('transactionHash', (txHash: string) => setPendingTx(txHash))
+										.on('transactionHash', (txHash: string) =>
+											setPendingTx(txHash),
+										)
 										.on('confirmation', handleConfirmations)
 										.on('error', clearPendingTx)
 									setPendingTx(true)
@@ -209,54 +226,54 @@ const ExternalLink = styled.a`
 `
 
 const SubmitButton = styled.button`
-    display: inline-flex;
-    appearance: none;
-    align-items: center;
-    justify-content: center;
-    user-select: none;
-    position: relative;
-    white-space: nowrap;
-    vertical-align: middle;
-    outline-offset: 2px;
-    width: 100%;
-    line-height: 1.2;
-    font-weight: ${(props) => props.theme.fontWeight.strong};
-    transition-property: all;
-    height: 50px;
-    min-width: 2.5rem;
+	display: inline-flex;
+	appearance: none;
+	align-items: center;
+	justify-content: center;
+	user-select: none;
+	position: relative;
+	white-space: nowrap;
+	vertical-align: middle;
+	outline-offset: 2px;
+	width: 100%;
+	line-height: 1.2;
+	font-weight: ${(props) => props.theme.fontWeight.strong};
+	transition-property: all;
+	height: 50px;
+	min-width: 2.5rem;
 	font-size: ${(props) => props.theme.fontSize.default};
-    padding-inline-start: 1rem;
-    padding-inline-end: 1rem;
+	padding-inline-start: 1rem;
+	padding-inline-end: 1rem;
 	border: none;
 	border-bottom: 1px solid ${(props) => props.theme.color.primary[400]};
 	box-shadow: ${(props) => props.theme.boxShadow.default};
-    background-color: ${(props) => props.theme.color.primary[100]};
-    outline: transparent solid 2px;
-    border-radius: 8px;
-    color: ${(props) => props.theme.color.text[100]};
-    opacity: ${(props) => (props.disabled ? 0.5 : 1)};
+	background-color: ${(props) => props.theme.color.primary[100]};
+	outline: transparent solid 2px;
+	border-radius: 8px;
+	color: ${(props) => props.theme.color.text[100]};
+	opacity: ${(props) => (props.disabled ? 0.5 : 1)};
 	position: relative;
 	transition: .5s;
 	overflow: hidden;
 
-    &:focus {
+	&:focus {
 		outline: 0;
 	}
 
-	  &:hover{
+	&:hover{
 		background: ${(props) => props.theme.color.primary[200]};
 		box-shadow: ${(props) =>
 			!props.disabled
 				? props.theme.boxShadow.hover
 				: props.theme.boxShadow.default};
 		cursor: pointer;
-	  }
 	}
+}
 
-	&:hover,
-	&:focus,
-	&:active {
-		color: ${(props) => (!props.disabled ? props.color : `${props.color}`)};
-		cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')} !important;
-	}
+&:hover,
+&:focus,
+&:active {
+	color: ${(props) => (!props.disabled ? props.color : `${props.color}`)};
+	cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')} !important;
+}
 `
