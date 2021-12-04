171d87e2887765b9725644a50da74573e0b60de5
Vitalsine <rob@creative-element.net>
2021-12-04 18:22:37 -0500

    light/dark colors WIP

---
+++ src/theme/lightColors.ts
@@ -0,0 +1,47 @@
+export const lightBackground = {
+  100: '#fff8ee', //light mode
+  200: '#50251c', //dark mode
+}
+
+export const lightPrimary = {
+  100: '#efeae7',
+  200: '#e7dfda',
+  300: '#ded4ce',
+  400: '#d6c9c2',
+  500: '#cebfb6',
+  600: '#c6b4a9',
+  700: '#bea99d',
+  800: '#b69e91',
+  900: '#ad9485',
+}
+
+export const lightSecondary = {
+  100: '#efd7d2',
+  200: '#e4bcb4',
+  300: '#daa195',
+  400: '#cf8677',
+  500: '#c56b59',
+  600: '#b55340',
+  700: '#974535',
+  800: '#79372a',
+  900: '#50251c',
+}
+
+export const lightText = {
+  100: '#50251c', //primary
+  200: '#aa9585', //secondary
+  300: '#f57d14', //hover link
+  400: '#ce6509', //active link
+}
+
+export const lightAccent = {
+  100: '#fde9d8',
+  200: '#fcd4b1', 
+  300: '#fabe89', 
+  400: '#f8a862', 
+  500: '#f7933b',
+  600: '#f57d14',
+  700: '#ce6509',
+  800: '#b05607',
+  900: '#894306',
+}
