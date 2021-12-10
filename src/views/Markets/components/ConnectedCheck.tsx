c38064495843cdd71fb0a963fa72d62834bb3ff5
Ben C <vexeth@protonmail.com>
2021-12-10 16:31:14 -0500

    Smoother connection check w/ useWallet rather than web3

--- src/views/Markets/components/ConnectedCheck.tsx
+++ src/views/Markets/components/ConnectedCheck.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import Config from '../../../bao/lib/config'
 import useModal from '../../../hooks/useModal'
-import useIsConnected from '../../../hooks/useIsConnected'
 import WalletProviderModal from '../../../components/WalletProviderModal'
 import { Button } from '../../../components/Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useWallet } from 'use-wallet'
 
 export const ConnectedCheck: React.FC = ({ children }) => {
 	const [onPresentWalletProviderModal] = useModal(
 		<WalletProviderModal />,
 		'provider',
 	)
-	const isConnected = useIsConnected()
+	const { account, ethereum }: any = useWallet()
 
-	return isConnected ? (
+	return account && ethereum.chainId === Config.defaultRpc.chainId ? (
 		<>{children}</>
 	) : (
 		<div style={{ width: '350px', margin: 'auto' }}>
