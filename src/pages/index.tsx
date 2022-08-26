6dd9d52faca021cde5464c327913c2db0987f0c9
Vitalsine85 <rob@creative-element.net>
2022-08-26 10:56:49 -0400

    Change homepage to Markets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,30 @@
-import Head from 'next/head'
+import PageHeader from '@/components/PageHeader'
+import { useMarkets } from '@/hooks/markets/useMarkets'
+import { useWeb3React } from '@web3-react/core'
+import { NextSeo } from 'next-seo'
 import React from 'react'
+import MarketList from './markets/components/MarketList'
+import OfflineMarketList from './markets/components/OfflineMarketList'
+import Overview from './markets/components/Overview'
 
-export default function Dashboard() {
+const Markets: React.FC = () => {
+	const markets = useMarkets()
+	const { account } = useWeb3React()
+
 	return (
-		<div className='mx-auto max-w-7xl py-4 sm:px-6 md:py-8 lg:py-12 lg:px-8'>
-			<Head>
-				<title>Bao | Dashboard</title>
-				<meta name='description' content='Bao' />
-				<meta key='twitter:description' name='twitter:description' content='Bao' />
-				<meta key='og:description' property='og:description' content='Bao' />
-			</Head>
-		</div>
+		<>
+			<NextSeo title={'Markets'} description={'Provide different collateral types to mint synthetics like baoUSD.'} />
+			<PageHeader title='Markets' />
+			{account ? (
+				<>
+					<Overview />
+					<MarketList markets={markets} />
+				</>
+			) : (
+				<OfflineMarketList markets={markets} />
+			)}
+		</>
 	)
 }
+
+export default Markets
