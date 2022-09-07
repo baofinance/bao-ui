dfc6eba49140468712d8abb2edfe9fa75a799cc8
Zachary Fogg <me@zfo.gg>
2022-09-07 19:14:40 -0400

    Initial rough work for tx popups.\nWorks for successful txs but not for failed txs yet, and needs styling and some testing.

--- src/components/TxPopup/TxPopup.tsx
+++ src/components/TxPopup/TxPopup.tsx
@@ -1,30 +1,39 @@
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useState, useEffect } from 'react'
-import cn from 'classnames'
 import { TransactionReceipt } from 'web3-core'
-import useTransactionProvider from '../../hooks/base/useTransactionProvider'
+import { useWeb3React } from '@web3-react/core'
+import Web3 from 'web3'
 import usePendingTransactions from '../../hooks/base/usePendingTransactions'
-
+import { waitTransaction, isSuccessfulTransaction } from './waitTransaction'
 
 interface PopupProps {
 	description?: any
 	receipt: TransactionReceipt
 }
 
-const Popup: React.FC<PopupProps> = ({description, receipt}) => {
-	console.log(description, receipt)
+const Popup: React.FC<PopupProps> = ({ description, receipt }) => {
+	const hash = receipt.transactionHash
+	const success = isSuccessfulTransaction(receipt)
+	const successText = success ? 'Succeeded' : 'Failed'
 	return (
 		<div className='bg-white shadow-lg mx-auto w-96 max-w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block'
 			id='static-example' role='alert' aria-live='assertive' aria-atomic='true' data-mdb-autohide='false'>
 			<div className='bg-white flex justify-between items-center py-2 px-3 bg-clip-padding border-b border-gray-200 rounded-t-lg'>
-				<p className='font-bold text-gray-500'>Transaction Succeeded</p>
+				<p className='font-bold text-gray-500'>Transaction {successText}</p>
 				<div className='flex items-center'>
-					<p className='text-gray-600 text-xs'>11 mins ago</p>
+					<p className='text-xs text-green'>
+						<FontAwesomeIcon icon={faCheck} size='2x' />
+					</p>
 				</div>
 			</div>
 			<div className='p-3 bg-white rounded-b-lg break-words text-gray-700'>
 				{description}
+				{' - '}
+				<a target='_blank' className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'
+					href={`https://etherscan.io/tx/${receipt.transactionHash}`}>
+				 {hash.slice(0,6)}...{hash.slice(-9,-1)} 
+				</a>
 			</div>
 		</div>
 	)
@@ -33,43 +42,54 @@ const Popup: React.FC<PopupProps> = ({description, receipt}) => {
 
 interface TxPopupProps {
 	children?: any
-	className?: any
 }
 
-const TxPopup: React.FC<TxPopupProps> = ({ children, content, className }) => {
+interface TxForPopup {
+	hash: string
+	description: string
+	receipt: TransactionReceipt
+}
+
+const TxPopup: React.FC<TxPopupProps> = ({ children }) => {
 	const pendingTxs = usePendingTransactions()
-	const { transactions } = useTransactionProvider()
 	const [seenTxs, setSeenTxs] = useState({})
 	const [popups, setPopups] = useState([])
+	const { library } = useWeb3React()
 
 	useEffect(() => {
+		const web3 = new Web3(library)
 		setSeenTxs((txs: any) => {
 			pendingTxs.map(tx => {
 				if (!txs[tx.hash]) {
-					console.log('adding popup for', tx.hash)
-					setPopups(pps => {
-						pps.push(tx)
-						return pps
+					waitTransaction(web3, tx.hash).then(receipt => {
+						console.log('got receipt, adding popup for', tx.hash)
+						setPopups(pps => {
+							const popupTx: TxForPopup = {
+								hash: tx.hash,
+								description: tx.description,
+								receipt,
+							}
+							pps.push(popupTx)
+							return pps
+						})
+						setTimeout(() => {
+							console.log('popup time over, removing popup for', tx.hash)
+							setPopups(pps => pps.filter(p => p.hash !== tx.hash))
+						}, 10000)
 					})
-					setTimeout(() => {
-						setPopups(pps => pps.filter(p => p.hash !== tx.hash))
-					}, 10000)
 				}
 				txs[tx.hash] = true
 			})
 			return txs
 		})
-	}, [pendingTxs, setSeenTxs, setPopups])
-
-	//console.log(popups)
+	}, [pendingTxs, setSeenTxs, setPopups, library])
 
 	return (
 		<div className='fixed flex items-center justify-center bottom-[0] w-[100%] pb-10'>
-			<div className={cn(['flex', 'justify-center', 'space-x-2', className])}>
-				{popups.map((tx, i) => {
-					console.log(tx)
-					return <Popup key={i} description={tx.description} />
-				})}
+			<div className='flex justify-center space-x-2'>
+				{popups.map((tx, i) => (
+					<Popup key={i} description={tx.description} receipt={tx.receipt} />
+				))}
 			</div>
 		</div>
 	)
