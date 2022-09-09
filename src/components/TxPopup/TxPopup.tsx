a8d18c121f180a5f637a3b7ef1b5e2baca427e0b
0xVital <rob@creative-element.net>
2022-09-09 18:07:54 -0400

    Removed new instance of Web3

--- src/components/TxPopup/TxPopup.tsx
+++ src/components/TxPopup/TxPopup.tsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import { useWeb3React } from '@web3-react/core'
+import 'animate.css/animate.min.css'
+import React, { useEffect, useState } from 'react'
 import { ReactNotifications, Store as NotifStore } from 'react-notifications-component'
 import 'react-notifications-component/dist/theme.css'
-import 'animate.css/animate.min.css'
-import { TransactionReceipt } from 'web3-core'
-import { useWeb3React } from '@web3-react/core'
-import Web3 from 'web3'
 import usePendingTransactions from '../../hooks/base/usePendingTransactions'
-import { waitTransaction, isSuccessfulTransaction } from './waitTransaction'
+import { isSuccessfulTransaction, waitTransaction } from './waitTransaction'
 
 const TxPopup: React.FC = () => {
 	const pendingTxs = usePendingTransactions()
@@ -14,11 +12,10 @@ const TxPopup: React.FC = () => {
 	const { library } = useWeb3React()
 
 	useEffect(() => {
-		const web3 = new Web3(library)
 		setSeenTxs((stxs: any) => {
 			pendingTxs.map(tx => {
 				if (!stxs[tx.hash]) {
-					waitTransaction(web3, tx.hash).then(receipt => {
+					waitTransaction(library, tx.hash).then(receipt => {
 						const success = isSuccessfulTransaction(receipt)
 						const successText = success ? 'Successful' : 'Failed'
 						NotifStore.addNotification({
@@ -26,10 +23,13 @@ const TxPopup: React.FC = () => {
 							message: (
 								<>
 									{tx.description} --{' '}
-									<a target='_blank' rel='noreferrer' href={`https://etherscan.io/tx/${receipt.transactionHash}`}
-										className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'
+									<a
+										target='_blank'
+										rel='noreferrer'
+										href={`https://etherscan.io/tx/${receipt.transactionHash}`}
+										className='text-blue-600 hover:text-blue-800 underline visited:text-purple-600'
 									>
-										{tx.hash.slice(0,6)}...{tx.hash.slice(-9,-1)} 
+										{tx.hash.slice(0, 6)}...{tx.hash.slice(-9, -1)}
 									</a>
 								</>
 							),
@@ -54,9 +54,7 @@ const TxPopup: React.FC = () => {
 		})
 	}, [pendingTxs, setSeenTxs, library])
 
-	return (
-		<ReactNotifications />
-	)
+	return <ReactNotifications />
 }
 
 export default TxPopup
--- src/components/TxPopup/waitTransaction.ts
+++ src/components/TxPopup/waitTransaction.ts
@@ -5,15 +5,13 @@
  * Based on https://raw.githubusercontent.com/Kaisle/await-transaction-mined/master/index.js
  */
 
-import Web3 from 'web3'
-
 const DEFAULT_INTERVAL = 500
 
 const DEFAULT_BLOCKS_TO_WAIT = 0
 
 interface Options {
-  interval: number
-  blocksToWait: number
+	interval: number
+	blocksToWait: number
 }
 
 /**
@@ -24,65 +22,61 @@ interface Options {
  * @param options Wait timers
  * @return Transaction receipt
  */
-export function waitTransaction(web3: Web3, txnHash: string | string[], options: Options = null): Promise<any> {
-  const interval = options && options.interval ? options.interval : DEFAULT_INTERVAL
-  const blocksToWait = options && options.blocksToWait ? options.blocksToWait : DEFAULT_BLOCKS_TO_WAIT
-  const transactionReceiptAsync = async function(txnHash, resolve, reject) {
-    try {
-      const receipt = web3.eth.getTransactionReceipt(txnHash)
-      if (!receipt) {
-        setTimeout(function () {
-          transactionReceiptAsync(txnHash, resolve, reject)
-        }, interval)
-      } else {
-        if (blocksToWait > 0) {
-          const resolvedReceipt = await receipt
-          if (!resolvedReceipt || !resolvedReceipt.blockNumber) {
-            setTimeout(function () {
-              transactionReceiptAsync(txnHash, resolve, reject)
-            }, interval)
-          }
-          else {
-            try {
-              const block = await web3.eth.getBlock(resolvedReceipt.blockNumber)
-              const current = await web3.eth.getBlock('latest')
-              if (current.number - block.number >= blocksToWait) {
-                const txn = await web3.eth.getTransaction(txnHash)
-                if (txn.blockNumber != null) resolve(resolvedReceipt)
-                else reject(new Error('Transaction with hash: ' + txnHash + ' ended up in an uncle block.'))
-              }
-              else setTimeout(function () {
-                transactionReceiptAsync(txnHash, resolve, reject)
-              }, interval)
-            }
-            catch (e) {
-              setTimeout(function () {
-                transactionReceiptAsync(txnHash, resolve, reject)
-              }, interval)
-            }
-          }
-        }
-        else resolve(receipt)
-      }
-    } catch(e) {
-      reject(e)
-    }
-  }
+export function waitTransaction(library: any, txnHash: string | string[], options: Options = null): Promise<any> {
+	const interval = options && options.interval ? options.interval : DEFAULT_INTERVAL
+	const blocksToWait = options && options.blocksToWait ? options.blocksToWait : DEFAULT_BLOCKS_TO_WAIT
+	const transactionReceiptAsync = async function (txnHash: any, resolve: any, reject: any) {
+		try {
+			const receipt = library.eth.getTransactionReceipt(txnHash)
+			if (!receipt) {
+				setTimeout(function () {
+					transactionReceiptAsync(txnHash, resolve, reject)
+				}, interval)
+			} else {
+				if (blocksToWait > 0) {
+					const resolvedReceipt = await receipt
+					if (!resolvedReceipt || !resolvedReceipt.blockNumber) {
+						setTimeout(function () {
+							transactionReceiptAsync(txnHash, resolve, reject)
+						}, interval)
+					} else {
+						try {
+							const block = await library.eth.getBlock(resolvedReceipt.blockNumber)
+							const current = await library.eth.getBlock('latest')
+							if (current.number - block.number >= blocksToWait) {
+								const txn = await library.eth.getTransaction(txnHash)
+								if (txn.blockNumber != null) resolve(resolvedReceipt)
+								else reject(new Error('Transaction with hash: ' + txnHash + ' ended up in an uncle block.'))
+							} else
+								setTimeout(function () {
+									transactionReceiptAsync(txnHash, resolve, reject)
+								}, interval)
+						} catch (e) {
+							setTimeout(function () {
+								transactionReceiptAsync(txnHash, resolve, reject)
+							}, interval)
+						}
+					}
+				} else resolve(receipt)
+			}
+		} catch (e) {
+			reject(e)
+		}
+	}
 
-  // Resolve multiple transactions once
-  if (Array.isArray(txnHash)) {
-    const promises = []
-    txnHash.forEach(function (oneTxHash) {
-      promises.push(waitTransaction(web3, oneTxHash, options))
-    })
-    return Promise.all(promises)
-  } else {
-    return new Promise(function (resolve, reject) {
-      transactionReceiptAsync(txnHash, resolve, reject)
-    })
-  }
+	// Resolve multiple transactions once
+	if (Array.isArray(txnHash)) {
+		const promises: any[] = []
+		txnHash.forEach(function (oneTxHash) {
+			promises.push(waitTransaction(library, oneTxHash, options))
+		})
+		return Promise.all(promises)
+	} else {
+		return new Promise(function (resolve, reject) {
+			transactionReceiptAsync(txnHash, resolve, reject)
+		})
+	}
 }
-
 
 /**
  * Check if the transaction was success based on the receipt.
@@ -92,9 +86,9 @@ export function waitTransaction(web3: Web3, txnHash: string | string[], options:
  * @param receipt Transaction receipt
  */
 export function isSuccessfulTransaction(receipt: any): boolean {
-  if(receipt.status == '0x1' || receipt.status == 1) {
-    return true
-  } else {
-    return false
-  }
+	if (receipt.status == '0x1' || receipt.status == 1) {
+		return true
+	} else {
+		return false
+	}
 }
