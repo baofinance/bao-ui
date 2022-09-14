b6f8879320e0df1a54487b75be52d2fbf7569eb8
Vital <34991917+Vitalsine85@users.noreply.github.com>
2022-09-14 18:24:15 -0400

    Cleaning up Migration UI

--- src/pages/migration/components/Distribution.tsx
+++ src/pages/migration/components/Distribution.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/future/image'
 import Badge from '@/components/Badge'
 import useLockedEarnings from '@/hooks/farms/useLockedEarnings'
 import { getDisplayBalance } from '@/utils/numberFormat'
@@ -12,86 +13,103 @@ const options = [
 		id: 1,
 		name: 'Migrate to veBAO',
 		shortDesc: 'Migrate your locked BAOv1 to veBAO.',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		desc: 'Choose to lock your balance directly into voting escrow BAO (veBAO) for a minimum of 3 years (the length of the unlock period). After you choose to lock into veBAO, you will no longer be able to participate in streaming of liquid BAOv2 tokens as all the tokens from your distribution will be converted to a locked veBAO balance. Locking into veBAO relinquishes access to your tokens as they will be locked, but you will get the benefits of having veBAO. veBAO gives ownership of a percentage of protocol fees and the ability to vote on governance proposals. Tokens locked into veBAO will not be subject to any slashing penalty.',
 	},
 	{
 		id: 2,
 		name: 'Claim Now',
 		shortDesc: 'Claim your unlocked BAO.',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		desc: 'Choose to perform a liquid distribution over 3 years. Over the course of the 3 years, 100% of your locked tokens will be distributed to you along the distribution curve defined below. Users can claim (this costs gas) as many times as you would like throughout this distribution, as long as you have accrued more tokens since your last claim. All tokens claimed using this method are not subject to any slashing penalty.',
 	},
 	{
 		id: 3,
 		name: 'End Distribution',
 		shortDesc: 'End your distribution.',
-		desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		desc: 'Users that perform a liquid distribution can choose to end their distribution early at any time after the distribution starts. A large slashing penalty will be applied to the tokens unlocked early, while all tokens already unlocked have no slash fee applied. The slash fee is based on the slash rate function defined below which will end their distribution and allow them to receive the rest of their tokens (minus slash fee) immediately. In order to activate this option the user has to manually start their distribution, then at some point afterwards decide to end it early.',
 	},
 ]
-
 const Distribution: React.FC = () => {
 	const [selectedOption, setSelectedOption] = useState(options[0])
 	const locks = useLockedEarnings()
 
 	return (
 		<>
-			<div className={`flex-flow mx-auto my-4 flex items-center justify-center`}>
-				<Typography className='px-2 text-sm text-text-200'>Your Locked BAO</Typography>
-				<Badge>{getDisplayBalance(locks)}</Badge>
+			<div className='border-b border-text-100 pb-5'>
+				<Typography variant='lg' className='text-lg font-medium leading-6 text-text-200'>
+					Select Your Distribution Method
+				</Typography>
+				<Typography variant='p' className='mt-2 text-text-100'>
+					Locked Bao holders have three options they can take with their locked positions. Any distribution will only begin once manually
+					initiated by the wallet owner. Please read the descriptions below very carefully. If you have any questions, please join our{' '}
+					<a href='https://discord.gg/BW3P62vJXT' target='_blank' rel='noreferrer noopener' className='font-medium hover:text-text-400'>
+						Discord
+					</a>{' '}
+					community!
+				</Typography>
 			</div>
-			<div className='my-4 flex w-1/4 flex-col'>
-				<Listbox value={selectedOption} onChange={setSelectedOption}>
-					{({ open }) => (
-						<>
-							<Listbox.Label className='mb-1 text-xs text-text-200'>Select your distribution option</Listbox.Label>
-							<div className='relative'>
-								<div className='inline-flex w-full rounded-md border-none shadow-sm'>
+			<div className='flex flex-row'>
+				<div className='my-4 flex w-1/4 flex-col'>
+					<Listbox value={selectedOption} onChange={setSelectedOption}>
+						{({ open }) => (
+							<>
+								<Listbox.Label className='mb-1 text-xs text-text-200'>Select your distribution option</Listbox.Label>
+								<div className='relative'>
 									<div className='inline-flex w-full rounded-md border-none shadow-sm'>
-										<div className='inline-flex w-full items-center rounded-l-md border border-primary-300 bg-primary-100 py-2 pl-3 pr-4 text-white shadow-sm'>
-											<CheckIcon className='h-5 w-5' aria-hidden='true' />
-											<p className='ml-2.5 text-sm font-medium'>{selectedOption.name}</p>
-										</div>
-										<Listbox.Button
-											className={
-												(classNames(open ? 'bg-primary-300 text-text-400' : 'text-text-100'),
-												'inline-flex items-center rounded-l-none rounded-r-md border border-primary-300 bg-primary-200 p-2 text-sm font-medium text-text-100 hover:bg-primary-300')
-											}
-										>
-											<ChevronDownIcon className='h-5 w-5 text-white' aria-hidden='true' />
-										</Listbox.Button>
-									</div>
-								</div>
-
-								<Transition show={open} as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0'>
-									<Listbox.Options className='absolute z-10 mt-2 w-72 origin-top-right divide-y divide-gray-200 overflow-hidden rounded-md bg-primary-200 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-										{options.map(option => (
-											<Listbox.Option
-												key={option.name}
-												className={({ active }) =>
-													classNames(active ? 'bg-primary-100 text-text-400' : 'text-text-100', 'cursor-default select-none p-4 text-sm')
+										<div className='inline-flex w-full rounded-md border-none shadow-sm'>
+											<div className='inline-flex w-full items-center rounded-l-md border border-primary-300 bg-primary-100 py-2 pl-3 pr-4 text-white shadow-sm'>
+												<CheckIcon className='h-5 w-5' aria-hidden='true' />
+												<p className='ml-2.5 text-sm font-medium'>{selectedOption.name}</p>
+											</div>
+											<Listbox.Button
+												className={
+													(classNames(open ? 'bg-primary-300 text-text-400' : 'text-text-100'),
+													'inline-flex items-center rounded-l-none rounded-r-md border border-primary-300 bg-primary-200 p-2 text-sm font-medium text-text-100 hover:bg-primary-300')
 												}
-												value={option}
 											>
-												{({ selected, active }) => (
-													<div className='flex flex-col'>
-														<div className='flex justify-between'>
-															<p className={selected ? 'font-semibold' : 'font-normal'}>{option.name}</p>
-															{selected ? (
-																<span className={active ? 'text-text-100' : 'text-text-200'}>
-																	<CheckIcon className='h-5 w-5' aria-hidden='true' />
-																</span>
-															) : null}
+												<ChevronDownIcon className='h-5 w-5 text-white' aria-hidden='true' />
+											</Listbox.Button>
+										</div>
+									</div>
+
+									<Transition show={open} as={Fragment} leave='transition ease-in duration-100' leaveFrom='opacity-100' leaveTo='opacity-0'>
+										<Listbox.Options className='absolute z-10 mt-2 w-72 origin-top-right divide-y divide-gray-200 overflow-hidden rounded-md bg-primary-200 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+											{options.map(option => (
+												<Listbox.Option
+													key={option.name}
+													className={({ active }) =>
+														classNames(active ? 'bg-primary-100 text-text-400' : 'text-text-100', 'cursor-default select-none p-4 text-sm')
+													}
+													value={option}
+												>
+													{({ selected, active }) => (
+														<div className='flex flex-col'>
+															<div className='flex justify-between'>
+																<p className={selected ? 'font-semibold' : 'font-normal'}>{option.name}</p>
+																{selected ? (
+																	<span className={active ? 'text-text-100' : 'text-text-200'}>
+																		<CheckIcon className='h-5 w-5' aria-hidden='true' />
+																	</span>
+																) : null}
+															</div>
+															<p className={classNames(active ? 'text-text-100' : 'text-text-200', 'mt-2')}>{option.shortDesc}</p>
 														</div>
-														<p className={classNames(active ? 'text-text-100' : 'text-text-200', 'mt-2')}>{option.shortDesc}</p>
-													</div>
-												)}
-											</Listbox.Option>
-										))}
-									</Listbox.Options>
-								</Transition>
-							</div>
-						</>
-					)}
-				</Listbox>
+													)}
+												</Listbox.Option>
+											))}
+										</Listbox.Options>
+									</Transition>
+								</div>
+							</>
+						)}
+					</Listbox>
+				</div>
+				<div className='mt-2 mb-1 flex w-full items-center justify-end gap-1'>
+					<Typography className='px-2 text-sm text-text-200'>Your Locked BAO Balance</Typography>
+					<div className='flex h-8 flex-row items-center justify-center gap-2 rounded border border-primary-400 bg-primary-100 px-2 py-4'>
+						<Image src='/images/tokens/BAO.png' height={24} width={24} alt='BAO' />
+						<Typography className='font-bold'>{getDisplayBalance(locks)}</Typography>
+					</div>
+				</div>
 			</div>
 			<div className='flex flex-row rounded bg-primary-100 p-4'>{selectedOption.desc}</div>
 		</>
--- src/pages/migration/components/Swapper.tsx
+++ src/pages/migration/components/Swapper.tsx
@@ -4,18 +4,21 @@ import Button from '@/components/Button'
 import Card from '@/components/Card'
 import Input from '@/components/Input'
 import Loader from '@/components/Loader'
+import Tooltipped from '@/components/Tooltipped'
 import Typography from '@/components/Typography'
 import useAllowance from '@/hooks/base/useAllowance'
 import useBao from '@/hooks/base/useBao'
 import useTokenBalance from '@/hooks/base/useTokenBalance'
 import useTransactionHandler from '@/hooks/base/useTransactionHandler'
 import { decimate, exponentiate, getDisplayBalance } from '@/utils/numberFormat'
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown, faArrowRightLong, faLeftRight, faShip, faSync } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ArrowLongRightIcon } from '@heroicons/react/20/solid'
 import { useWeb3React } from '@web3-react/core'
 import BigNumber from 'bignumber.js'
 import Image from 'next/future/image'
 import React, { useMemo, useState } from 'react'
+import { buildStyles, CircularProgressbarWithChildren } from 'react-circular-progressbar'
 import { isDesktop } from 'react-device-detect'
 
 const Swapper: React.FC = () => {
@@ -25,50 +28,107 @@ const Swapper: React.FC = () => {
 
 	return (
 		<>
-			<Card className={`${isDesktop && 'mx-auto max-w-[80%]'}`}>
-				<Card.Body>
-					<Typography variant='sm' className='float-right mb-1'>
-						Balance: {getDisplayBalance(baov1Balance).toString()} BAOv1
-					</Typography>
-					<Input
-						onSelectMax={() => setInputVal(decimate(baov1Balance).toString())}
-						onChange={(e: { currentTarget: { value: React.SetStateAction<string> } }) => setInputVal(e.currentTarget.value)}
-						value={inputVal}
-						label={
-							<div className='flex flex-row items-center pl-2 pr-4'>
-								<div className='flex w-6 items-center justify-center'>
-									<Image src='/images/tokens/BAO.png' height={32} width={32} alt='BAO' />
-									<Typography variant='sm'>v1</Typography>
+			<div className='border-b border-text-100 pb-5'>
+				<Typography variant='lg' className='text-lg font-medium leading-6 text-text-100'>
+					Exchange Unlocked BAO
+				</Typography>
+				<Typography variant='p' className='mt-2 text-text-200'>
+					All tokens whether unlocked/circulating or locked will be converted and given at a supply reduction of 1,000:1 relative to the
+					~1.5 trillion BAOv1 tokens. The new initial supply will be ~1.5 billion BAOv2 and will increase dependent upon issuance from
+					voting escrow emissions.
+				</Typography>
+			</div>
+			<div className='flex flex-row gap-4'>
+				<div className='flex w-3/4 flex-col'>
+					<Card className={`mt-8 h-[300px]`}>
+						<Card.Body>
+							<div className='mt-2 mb-1 flex w-full items-center justify-end gap-1'>
+								<Typography variant='sm' className='text-text-200'>
+									Wallet
+								</Typography>
+								<Typography variant='sm'>{getDisplayBalance(baov1Balance).toString()} BAO v1</Typography>
+							</div>
+							<Input
+								onSelectMax={() => setInputVal(decimate(baov1Balance).toString())}
+								onChange={(e: { currentTarget: { value: React.SetStateAction<string> } }) => setInputVal(e.currentTarget.value)}
+								value={inputVal}
+								label={
+									<div className='flex flex-row items-center pl-2 pr-4'>
+										<div className='flex w-6 items-center justify-center'>
+											<Image src='/images/tokens/BAO.png' height={32} width={32} alt='BAO' />
+											<Typography variant='sm' className='font-medium'>
+												v1
+											</Typography>
+										</div>
+									</div>
+								}
+							/>
+							<div className='mt-4 block select-none text-center'>
+								<span className='mb-2 rounded-full border-none bg-primary-300 p-2 text-lg'>
+									<FontAwesomeIcon icon={faArrowDown} size='sm' className='m-auto' />
+								</span>
+							</div>
+							<div className='flex w-full flex-row'>
+								<div className='float-left mb-1 flex w-full items-center justify-end gap-1'>
+									<Typography variant='sm' className='text-text-200'>
+										Wallet
+									</Typography>
+									<Typography variant='sm'>{getDisplayBalance(baov1Balance).toString()} BAO v2</Typography>
 								</div>
 							</div>
-						}
-					/>
-					<div className='mt-4 block select-none text-center'>
-						<span className='mb-2 rounded-full border-none bg-primary-300 p-2 text-lg'>
-							<FontAwesomeIcon icon={faArrowDown} size='sm' className='m-auto' />
-						</span>
-					</div>
-					<div className='h-4' />
-					<Input
-						onSelectMax={null}
-						onChange={(e: { currentTarget: { value: React.SetStateAction<string> } }) => setInputVal(e.currentTarget.value)}
-						disabled={true}
-						value={inputVal && new BigNumber(inputVal).times(0.001).toString()}
-						label={
-							<div className='flex flex-row items-center pl-2 pr-4'>
-								<div className='flex w-6 items-center justify-center'>
-									<Image src='/images/tokens/BAO.png' height={32} width={32} alt='BAO' />
-									<Typography variant='sm'>v2</Typography>
+							<Input
+								onSelectMax={null}
+								onChange={(e: { currentTarget: { value: React.SetStateAction<string> } }) => setInputVal(e.currentTarget.value)}
+								disabled={true}
+								value={inputVal && new BigNumber(inputVal).times(0.001).toString()}
+								label={
+									<div className='flex flex-row items-center pl-2 pr-4'>
+										<div className='flex w-6 items-center justify-center'>
+											<Image src='/images/tokens/BAO.png' height={32} width={32} alt='BAO' />
+											<Typography variant='sm' className='font-medium'>
+												v2
+											</Typography>
+										</div>
+									</div>
+								}
+							/>
+							<div className='h-4' />
+						</Card.Body>
+						<Card.Actions>
+							<SwapperButton inputVal={inputVal} maxValue={decimate(baov1Balance)} />
+						</Card.Actions>
+					</Card>
+				</div>
+				<div className='flex w-1/2 flex-col'>
+					<Card className='mt-8 h-[300px] w-full'>
+						<Card.Header header='Migration Progress' />
+						<div className='m-auto w-[200px]'>
+							<CircularProgressbarWithChildren
+								value={50}
+								strokeWidth={10}
+								styles={buildStyles({
+									strokeLinecap: 'butt',
+									pathColor: `green`,
+								})}
+							>
+								<div className='max-w-[16.6666666667%] basis-[16.6666666667%]'>
+									<div className='relative left-1/2 h-[130px] w-[130px] -translate-x-1/2 rounded-full bg-primary-100'>
+										<div
+											className='absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center rounded-full p-1'
+											style={{ marginTop: '15px' }}
+										>
+											<Typography variant='sm' className='text-text-200'>
+												BAOv1 Redeemed
+											</Typography>
+											<Typography>50%</Typography>
+										</div>
+									</div>
 								</div>
-							</div>
-						}
-					/>
-					<div className='h-4' />
-				</Card.Body>
-				<Card.Actions>
-					<SwapperButton inputVal={inputVal} maxValue={decimate(baov1Balance)} />
-				</Card.Actions>
-			</Card>
+							</CircularProgressbarWithChildren>
+						</div>
+					</Card>
+				</div>
+			</div>
 		</>
 	)
 }
@@ -112,7 +172,12 @@ const SwapperButton: React.FC<SwapperButtonProps> = ({ inputVal, maxValue }: Swa
 	}
 
 	const isDisabled = useMemo(
-		() => typeof pendingTx === 'string' || pendingTx || new BigNumber(inputVal).isNaN() || new BigNumber(inputVal).gt(maxValue),
+		() =>
+			typeof pendingTx === 'string' ||
+			pendingTx ||
+			new BigNumber(inputVal).isNaN() ||
+			new BigNumber(inputVal).gt(maxValue) ||
+			new BigNumber(maxValue).lte(0),
 		[pendingTx, inputVal],
 	)
 
