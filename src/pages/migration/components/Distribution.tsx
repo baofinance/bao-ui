08e08da77b73bc438d49f6b8ba210067073e5a2a
Vital <34991917+Vitalsine85@users.noreply.github.com>
2022-09-15 00:41:14 -0400

    Clean up Migration page

--- src/pages/migration/components/Distribution.tsx
+++ src/pages/migration/components/Distribution.tsx
@@ -1,31 +1,105 @@
-import Image from 'next/future/image'
-import Badge from '@/components/Badge'
+import Typography from '@/components/Typography'
 import useLockedEarnings from '@/hooks/farms/useLockedEarnings'
 import { getDisplayBalance } from '@/utils/numberFormat'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
-import { Typography } from '@material-tailwind/react'
 import classNames from 'classnames'
+import Image from 'next/future/image'
 import React, { Fragment, useState } from 'react'
+import 'katex/dist/katex.min.css'
+import Latex from 'react-latex-next'
 
 const options = [
 	{
 		id: 1,
 		name: 'Migrate to veBAO',
 		shortDesc: 'Migrate your locked BAOv1 to veBAO.',
-		desc: 'Choose to lock your balance directly into voting escrow BAO (veBAO) for a minimum of 3 years (the length of the unlock period). After you choose to lock into veBAO, you will no longer be able to participate in streaming of liquid BAOv2 tokens as all the tokens from your distribution will be converted to a locked veBAO balance. Locking into veBAO relinquishes access to your tokens as they will be locked, but you will get the benefits of having veBAO. veBAO gives ownership of a percentage of protocol fees and the ability to vote on governance proposals. Tokens locked into veBAO will not be subject to any slashing penalty.',
+		desc: (
+			<>
+				<Typography variant='p'>
+					Choose to lock your balance directly into voting escrow BAO (veBAO) for a minimum of 3 years (the length of the unlock period).
+					After you choose to lock into veBAO, you will no longer be able to participate in streaming of liquid BAOv2 tokens as all the
+					tokens from your distribution will be converted to a locked veBAO balance. Locking into veBAO relinquishes access to your tokens
+					as they will be locked, but you will get the benefits of having veBAO. veBAO gives ownership of a percentage of protocol fees and
+					the ability to vote on governance proposals. Tokens locked into veBAO will not be subject to any slashing penalty.
+				</Typography>
+			</>
+		),
 	},
 	{
 		id: 2,
 		name: 'Claim Now',
 		shortDesc: 'Claim your unlocked BAO.',
-		desc: 'Choose to perform a liquid distribution over 3 years. Over the course of the 3 years, 100% of your locked tokens will be distributed to you along the distribution curve defined below. Users can claim (this costs gas) as many times as you would like throughout this distribution, as long as you have accrued more tokens since your last claim. All tokens claimed using this method are not subject to any slashing penalty.',
+		desc: (
+			<>
+				<div className='flex flex-col'></div>
+				<div className='flex flex-col'>
+					<div className='flex flex-row'>
+						<Typography variant='p'>
+							Choose to perform a liquid distribution over 3 years. Over the course of the 3 years, 100% of your locked tokens will be
+							distributed to you along the distribution curve defined below. You can claim as many times as you would like throughout this
+							distribution, as long as you have accrued more tokens since your last claim. All tokens claimed using this method are not
+							subject to any slashing penalty. You may still choose to lock into veBAO or end your distribution at any time during the 3
+							year period.
+						</Typography>
+					</div>
+						<div className='flex flex-col m-auto'>
+							<Latex>{`\\(P_u = Percent \\ Unlocked \\)`}</Latex>
+							<Latex>{`\\(x = Days \\)`}</Latex>
+							<div className='h-2' />
+							<Latex>{`\\(P_{u}(x) \\begin{cases} \\begin{matrix} (\\frac{2x}{219})^2 & 0 \\leq x \\leq 1095 \\\\ 100 & x > 1095 \\end{matrix} \\end{cases} \\)`}</Latex>
+						</div>
+						<div className='flex w-1/2 flex-col items-center justify-center mt-4 m-auto'>
+							<img
+								src='https://global.discourse-cdn.com/standard10/uploads/bao/original/1X/672bf049e86f377e5129b84931bba7933e324bcc.png'
+								className='h-[200px] rounded'
+							/>
+							<Typography variant='xs' className='mt-2 text-center text-text-200'>
+								A graph showing how BAO will unlock over time following.
+							</Typography>
+						</div>
+					</div>
+			</>
+		),
 	},
 	{
 		id: 3,
 		name: 'End Distribution',
 		shortDesc: 'End your distribution.',
-		desc: 'Users that perform a liquid distribution can choose to end their distribution early at any time after the distribution starts. A large slashing penalty will be applied to the tokens unlocked early, while all tokens already unlocked have no slash fee applied. The slash fee is based on the slash rate function defined below which will end their distribution and allow them to receive the rest of their tokens (minus slash fee) immediately. In order to activate this option the user has to manually start their distribution, then at some point afterwards decide to end it early.',
+		desc: (
+			<>
+				<div className='flex flex-col'>
+					<Typography variant='p'>
+						Users that perform a liquid distribution can choose to end their distribution early at any time after the distribution starts. A
+						large slashing penalty will be applied to the tokens unlocked early, while all tokens already unlocked have no slash fee
+						applied. The slash fee is based on the slash rate function defined below which will end their distribution and allow them to
+						receive the rest of their tokens (minus slash fee) immediately.
+					</Typography>
+					<Typography variant='lg' className='mt-2 mb-2 font-bold'>
+						Slash Function
+					</Typography>
+					<code>X = days since start of distribution</code>
+					<code>{'0 <= X <= 365 || (100 -.01369863013x)'}</code>
+					<code>{'365 <X <= 1095 || 95'}</code>
+					<Typography variant='p'>
+						The slash function starts at the same date the address selects to start their BAO distribution. The slash function starts at
+						100% on day 0, and approaches a constant 95% slash on day 365 and thereafter. On the day that the user chooses to end their
+						distribution early, the slash function defines how much of their remaining distribution will be slashed. The user will receive
+						the remainder of this percentage of their remaining distribution immediately.
+					</Typography>
+					<div className='m-auto mt-2 flex w-1/2 flex-col items-center justify-center'>
+						<img
+							src='https://global.discourse-cdn.com/standard10/uploads/bao/original/1X/d0683e4c31a1d5cbfdf4a1a23f76325ca884ee43.gif'
+							className='max-h-[400px] w-min rounded'
+						/>
+						<Typography variant='xs' className='mt-2 text-center text-text-200'>
+							In the graph above, the blue points are the state of both curves respectively. The orange point is the percentage of their
+							total distribution that the user will be able to claim if they choose to end their distribution at that point.
+						</Typography>
+					</div>
+				</div>
+			</>
+		),
 	},
 ]
 const Distribution: React.FC = () => {
