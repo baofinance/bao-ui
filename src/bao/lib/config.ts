1a4585b0f93ad2c4ea21971c7b5088bf56220732
Zachary Fogg <me@zfo.gg>
2022-09-26 10:09:36 -0400

    oops forgot this last commit

--- src/bao/lib/config.ts
+++ src/bao/lib/config.ts
@@ -120,6 +120,12 @@ export default {
 				abi: 'feeDistributor.json',
 			},
 		},
+		dai: {
+			1: {
+				address: '0x6b175474e89094c44da98b954eedeac495271d0f',
+				abi: 'dai.json',
+			},
+		},
 	},
 	subgraphs: {
 		sushiExchange: {
