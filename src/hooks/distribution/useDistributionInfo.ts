fb73c0166e024c2391f9b614a8d90b076dce1f7a
Zachary Fogg <me@zfo.gg>
2022-11-19 15:46:58 -0500

    fixed my useDistributionInfo hook

--- src/hooks/distribution/useDistributionInfo.ts
+++ src/hooks/distribution/useDistributionInfo.ts
@@ -6,9 +6,7 @@ import { exponentiate } from '@/utils/numberFormat'
 import { useQuery } from '@tanstack/react-query'
 import { useWeb3React } from '@web3-react/core'
 import { BigNumber } from 'ethers'
-import useBao from '../base/useBao'
 import useContract from '../base/useContract'
-import useTransactionHandler from '../base/useTransactionHandler'
 
 type DistributionInfo = {
 	dateStarted: BigNumber
@@ -21,29 +19,27 @@ type DistributionInfo = {
 
 const useDistribtuionInfo = (): DistributionInfo => {
 	const { library, account, chainId } = useWeb3React()
-	const bao = useBao()
-	const { txSuccess } = useTransactionHandler()
 	const distribution = useContract<BaoDistribution>('BaoDistribution')
 
-	const enabled = !!bao && !!account && !!distribution
+	const enabled = !!account && !!distribution
 	const { data: distributionInfo, refetch } = useQuery(
 		['@/hooks/vebao/useAccountDistribution', providerKey(library, account, chainId)],
 		async () => {
-			const info = distribution ? await distribution.distributions(account) : null
-			const claimable = distribution ? await distribution.claimable(account, 0) : null
+			const info = await distribution.distributions(account)
+			const claimable = await distribution.claimable(account, 0)
 			const block = await library?.getBlock()
 			const dateStarted = info.dateStarted.mul(1000).toNumber()
 			const dateNow = block.timestamp * 1000
 			const daysDiff = Math.floor((dateNow - dateStarted) / 86400000)
-			const curve = distribution ? await distribution.distCurve(info.amountOwedTotal, exponentiate(BigNumber.from(daysDiff))) : null
+			const curve = await distribution.distCurve(info.amountOwedTotal, exponentiate(BigNumber.from(daysDiff)))
 
 			return {
-				dateStarted: info ? info.dateStarted : BigNumber.from(0),
-				dateEnded: info ? info.dateEnded : BigNumber.from(0),
-				lastClaim: info ? info.lastClaim : BigNumber.from(0),
-				amountOwedTotal: info ? info.amountOwedTotal : BigNumber.from(0),
-				claimable: claimable ? claimable : BigNumber.from(0),
-				curve: curve ? curve : BigNumber.from(0),
+				dateStarted: info.dateStarted,
+				dateEnded: info.dateEnded,
+				lastClaim: info.lastClaim,
+				amountOwedTotal: info.amountOwedTotal,
+				claimable,
+				curve,
 			}
 		},
 		{
