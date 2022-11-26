335beed1430868855cefe4a056e849dff0b7a810
Zachary Fogg <me@zfo.gg>
2022-11-25 20:53:22 -0500

    removed unneeded deps

--- src/pages/farms/index.tsx
+++ src/pages/farms/index.tsx
@@ -5,9 +5,6 @@ import PageHeader from '@/components/PageHeader'
 
 import Balances from './components/Balances'
 import FarmList from './components/FarmList'
-import Card from '@/components/Card'
-import Typography from '@/components/Typography'
-import Link from 'next/link'
 
 const Farms: React.FC = () => {
 	return (
