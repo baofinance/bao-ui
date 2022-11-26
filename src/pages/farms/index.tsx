8e4698300f60bfa49b41c81fd23bec6bdaf5e141
Zachary Fogg <me@zfo.gg>
2022-11-25 20:52:19 -0500

    removed the farms banner warning to harvest so we can go live for vebao and gauges cuz it is not relevant anymore

--- src/pages/farms/index.tsx
+++ src/pages/farms/index.tsx
@@ -14,26 +14,6 @@ const Farms: React.FC = () => {
 		<>
 			<NextSeo title={'Farms'} description={'Stake LP tokens to earn BAO.'} />
 			<PageHeader title='Farms' />
-			<Card className='flex'>
-				<div className='flex-row'>
-					<Typography variant='lg' className='mr-2 inline font-bold text-red'>
-						Attention:
-					</Typography>
-					<Typography className='inline'>
-						Withdraw your LP tokens and migrate to{' '}
-						<Link href='/distribution/'>
-							<a className='font-bold hover:text-text-400'>Gauges</a>
-						</Link>{' '}
-						if you wish to continue farming BAO. If you didn{"'"}t harvest before the November 19 snapshot of locked BAO, you will only be
-						able to migrate the balance you had at the time of the snapshot. Any liquid BAO collected from harvests post-snapshot can be
-						swapped with the{' '}
-						<Link href='/distribution/'>
-							<a className='font-bold hover:text-text-400'>migration swapper</a>
-						</Link>
-						.
-					</Typography>
-				</div>
-			</Card>
 			<Balances />
 			<FarmList />
 		</>
