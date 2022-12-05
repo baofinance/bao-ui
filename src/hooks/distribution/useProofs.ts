8792cd16c29901461b14a2a17259bfdc5c9a5f38
Vital <rob@creative-element.net>
2022-12-05 15:32:26 -0500

    fix api error

--- src/hooks/distribution/useProofs.ts
+++ src/hooks/distribution/useProofs.ts
@@ -8,9 +8,7 @@ const useProofs = () => {
 	const { data: merkleLeaf } = useQuery(
 		['@/hooks/distribution/useProofs', { enabled, account }],
 		async () => {
-			const leafResponse = await fetch(
-				`https://bao-dist-api.herokuapp.com/https://staging.app.bao.finance/api/vebao/distribution/proof/${account}/`,
-			)
+			const leafResponse = await fetch(`https://bao-dist-api.herokuapp.com/addresses?address=${account.toLowerCase()}`)
 			if (leafResponse.status !== 200) {
 				const { error } = await leafResponse.json()
 				throw new Error(`${error.code} - ${error.message}`)
