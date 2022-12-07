0d17b135b6e7bcd0d44c6d3da587755fd07c3b4c
Zachary Fogg <me@zfo.gg>
2022-12-07 17:31:32 -0500

    more slash math to show what u get from slashing also a bit more text containing warnings and reassurances

--- src/pages/distribution/components/Locked.tsx
+++ src/pages/distribution/components/Locked.tsx
@@ -296,12 +296,17 @@ const Migration: React.FC = () => {
 		)
 	}
 
+	//return (
+	//	<div className='flex flex-col items-center'>
+	//		<div className='pt-4 md:w-4/5'>{distElement}</div>
+	//	</div>
+	//)
 	return (
-		<>
-			<div className='flex flex-col items-center'>
-				<div className='pt-4 md:w-4/5'>{distElement}</div>
+		<div className='flex flex-col items-center'>
+			<div className='pt-4 md:w-4/5'>
+				<End />
 			</div>
-		</>
+		</div>
 	)
 }
 
