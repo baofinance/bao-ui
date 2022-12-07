eba4b211a1541a806680cb1f9376e10866a30bd0
Vital <rob@creative-element.net>
2022-12-06 23:06:48 -0500

    ver update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,7 @@ import TransactionProvider from '@/contexts/Transactions'
 import TxPopup from '@/components/TxPopup'
 import '@/components/TxPopup/styles.css'
 
-console.log('v1.1.6')
+console.log('v1.1.5')
 
 function getLibrary(provider: any): Web3Provider {
 	const library = new Web3Provider(provider)
