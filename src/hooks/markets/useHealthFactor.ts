7fe71128b4c487e142446eef95bb0fb5a477c393
Vital <rob@creative-element.net>
2022-12-23 10:27:12 -0500

    Rollback health factor calculation

--- src/hooks/markets/useHealthFactor.ts
+++ src/hooks/markets/useHealthFactor.ts
@@ -1,64 +1,59 @@
 import { useWeb3React } from '@web3-react/core'
 import { BigNumber } from 'ethers'
+import { useCallback, useEffect, useState } from 'react'
 import Multicall from '@/utils/multicall'
 import useBao from '../base/useBao'
 import { useAccountLiquidity } from './useAccountLiquidity'
 import { useAccountMarkets } from './useMarkets'
 import { useMarketPrices } from './usePrices'
-import { providerKey } from '@/utils/index'
-import { useQuery } from '@tanstack/react-query'
-import { useBlockUpdater } from '@/hooks/base/useBlock'
-import { useTxReceiptUpdater } from '@/hooks/base/useTransactionProvider'
-import { decimate } from '@/utils/numberFormat'
+import { formatUnits, parseUnits } from 'ethers/lib/utils'
 
 const useHealthFactor = () => {
+	const [healthFactor, setHealthFactor] = useState<BigNumber | undefined>()
 	const bao = useBao()
-	const { library, account, chainId } = useWeb3React()
+	const { account } = useWeb3React()
 	const markets = useAccountMarkets()
 	const accountLiquidity = useAccountLiquidity()
 	const { prices } = useMarketPrices()
 
-	const enabled = !!bao && !!account && !!markets && !!accountLiquidity && !!prices
-	const { data: healthFactor, refetch } = useQuery(
-		['@/hooks/markets/useHealthFactor', providerKey(library, account, chainId), { enabled, accountLiquidity, prices }],
-		async () => {
-			const usdBorrow = accountLiquidity.usdBorrow
-			const _markets = markets.filter(market => !market.isSynth)
+	const fetchHealthFactor = useCallback(async () => {
+		const usdBorrow = BigNumber.from(parseUnits(accountLiquidity.usdBorrow.toString()))
+		const _markets = markets.filter(market => !market.isSynth)
 
-			const balanceQuery = Multicall.createCallContext(
-				_markets.map(market => ({
-					contract: market.marketContract,
-					ref: market.marketAddress,
-					calls: [{ method: 'balanceOfUnderlying', params: [account] }],
-				})),
+		const balanceQuery = Multicall.createCallContext(
+			_markets.map(market => ({
+				contract: market.marketContract,
+				ref: market.marketAddress,
+				calls: [{ method: 'balanceOfUnderlying', params: [account] }],
+			})),
+		)
+		const balanceRes = Multicall.parseCallResults(await bao.multicall.call(balanceQuery))
+
+		if (Object.keys(balanceRes).length === 0) return setHealthFactor(BigNumber.from(0))
+
+		const collateralSummation = _markets.reduce((prev, cur) => {
+			return prev.add(
+				BigNumber.from(prices[cur.marketAddress])
+					.div(BigNumber.from(10).pow(36 - cur.underlyingDecimals))
+					.mul(parseUnits(balanceRes[cur.marketAddress][0].values[0].toString()))
+					.div(BigNumber.from(10).pow(cur.underlyingDecimals))
+					.mul(parseUnits(cur.collateralFactor.toString())),
 			)
-			const balanceRes = Multicall.parseCallResults(await bao.multicall.call(balanceQuery))
+		}, BigNumber.from(0))
 
-			if (Object.keys(balanceRes).length === 0) return BigNumber.from(0)
+		try {
+			const _healthFactor = collateralSummation.div(usdBorrow.toString())
+			setHealthFactor(_healthFactor)
+		} catch {
+			setHealthFactor(BigNumber.from(0))
+		}
+	}, [markets, accountLiquidity, bao, account, prices])
 
-			const collateralSummation = _markets.reduce((prev, cur) => {
-				const next = BigNumber.from(prices[cur.marketAddress]).mul(balanceRes[cur.marketAddress][0].values[0]).mul(cur.collateralFactor)
-				return prev.add(decimate(next, cur.underlyingDecimals))
-			}, BigNumber.from(0))
+	useEffect(() => {
+		if (!(markets && accountLiquidity && bao && account && prices)) return
 
-			try {
-				const _healthFactor = collateralSummation.div(usdBorrow)
-				return _healthFactor
-			} catch {
-				return BigNumber.from(0)
-			}
-		},
-		{
-			enabled,
-			placeholderData: BigNumber.from('0'),
-		},
-	)
-
-	const _refetch = () => {
-		if (enabled) refetch()
-	}
-	useBlockUpdater(_refetch, 10)
-	useTxReceiptUpdater(_refetch)
+		fetchHealthFactor()
+	}, [markets, accountLiquidity, bao, account, prices, fetchHealthFactor])
 
 	return healthFactor
 }
