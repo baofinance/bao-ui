83cd6f9192c8ca2d435d8405059dc249d848c761
Vital <rob@creative-element.net>
2023-01-04 16:09:40 -0500

    Add popups for reverted or rejected transactions.

--- src/hooks/base/useTransactionHandler.ts
+++ src/hooks/base/useTransactionHandler.ts
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { EthersError, getParsedEthersError } from '@enzoferey/ethers-error-parser'
 import { TransactionReceipt } from '@ethersproject/providers'
 import { ContractTransaction } from 'ethers'
+import { useState } from 'react'
+import { Store as NotifStore } from 'react-notifications-component'
 import useTransactionProvider from './useTransactionProvider'
 
 const useTransactionHandler = () => {
@@ -26,13 +28,54 @@ const useTransactionHandler = () => {
 	}
 
 	const handleTx = async (_tx: Promise<ContractTransaction>, description: string, cb?: () => void) => {
-		const tx = await _tx
-		handlePendingTx(tx.hash, description)
-		setPendingTx(true)
-		const receipt = await tx.wait()
-		handleReceipt(receipt)
-		if (cb) cb()
-		setTxSuccess(receipt.status === 1)
+		try {
+			const tx = await _tx
+			handlePendingTx(tx.hash, description)
+			setPendingTx(true)
+			const receipt = await tx.wait()
+			handleReceipt(receipt)
+			if (cb) cb()
+			setTxSuccess(receipt.status === 1)
+		} catch (e) {
+			const error = getParsedEthersError(e as EthersError)
+			const context = error.context.toString()
+
+			if (context.includes('rejected')) {
+				NotifStore.addNotification({
+					title: `Transaction Failed`,
+					message: `User rejected transaction`,
+					type: 'danger',
+					insert: 'top',
+					container: 'bottom-right',
+					animationIn: ['animate__animated', 'animate__fadeIn'],
+					animationOut: ['animate__animated', 'animate__fadeOut'],
+					dismiss: {
+						pauseOnHover: true,
+						duration: 7000,
+						onScreen: true,
+						click: false, // so one can click the etherscan link
+						touch: false, // so one can click the etherscan link
+					},
+				})
+			} else {
+				NotifStore.addNotification({
+					title: `Transaction Reverted`,
+					message: `${error.context}`,
+					type: 'danger',
+					insert: 'top',
+					container: 'bottom-right',
+					animationIn: ['animate__animated', 'animate__fadeIn'],
+					animationOut: ['animate__animated', 'animate__fadeOut'],
+					dismiss: {
+						pauseOnHover: true,
+						duration: 7000,
+						onScreen: true,
+						click: false, // so one can click the etherscan link
+						touch: false, // so one can click the etherscan link
+					},
+				})
+			}
+		}
 	}
 
 	return {
