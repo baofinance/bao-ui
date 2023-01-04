3b2acff5b2536b723e1ae947f3c081c267184da4
Vital <rob@creative-element.net>
2023-01-04 16:54:32 -0500

    Vote slider fix

--- src/pages/gauges/components/Actions.tsx
+++ src/pages/gauges/components/Actions.tsx
@@ -306,6 +306,9 @@ export const Vote: React.FC<VoteProps> = ({ gauge, tvl, rewardsValue }) => {
 		[setVal],
 	)
 
+	console.log(votingPowerAllocated.toString())
+	console.log(userSlopes ? userSlopes.power.toString() : 0)
+
 	return (
 		<>
 			<Modal.Body>
@@ -362,7 +365,11 @@ export const Vote: React.FC<VoteProps> = ({ gauge, tvl, rewardsValue }) => {
 								userSlopes && BigNumber.from(userSlopes.power) !== BigNumber.from(0) ? userSlopes.power.div(100).toString() : val
 							}
 							min={0}
-							max={userSlopes && lockInfo && votingPowerAllocated.div(100).sub(userSlopes.power.div(100)).toString()}
+							max={
+								userSlopes && lockInfo && userSlopes.power.div(100) === BigNumber.from(100)
+									? BigNumber.from(100).toString()
+									: votingPowerAllocated.div(100).sub(userSlopes.power.div(100)).toString()
+							}
 							value={val}
 							className='form-range border-r-1 h-6 w-full appearance-none rounded-md rounded-r-none border-background-100 bg-primary-300 p-2 focus:shadow-none focus:outline-none focus:ring-0'
 							onChange={handleChange}
@@ -371,24 +378,20 @@ export const Vote: React.FC<VoteProps> = ({ gauge, tvl, rewardsValue }) => {
 						<input
 							type='number'
 							id='points'
+							disabled={true}
 							onChange={handleChange}
 							placeholder={val.toString()}
 							value={val}
-							min={userSlopes ? userSlopes.power.div(100).toString() : 0}
+							min={0}
 							max={
-								votingPowerAllocated.sub(BigNumber.from(100)).eq(0)
-									? 0
-									: userSlopes &&
-									  lockInfo &&
-									  BigNumber.from(100)
-											.sub(votingPowerAllocated.div(BigNumber.from(100)))
-											.add(userSlopes.power.div(100))
-											.toString()
+								userSlopes && lockInfo && userSlopes.power.div(100) === BigNumber.from(100)
+									? BigNumber.from(100).toString()
+									: votingPowerAllocated.div(100).sub(userSlopes.power.div(100)).toString()
 							}
 							className='relative -mr-1 h-6 w-10 min-w-0
 				appearance-none rounded border-solid border-inherit border-primary-500 bg-primary-100 pl-2 text-end 
 				align-middle outline-none outline outline-2 outline-offset-2 transition-all
-				 duration-200 disabled:text-text-200 md:text-sm'
+				 duration-200 disabled:text-text-100 md:text-sm'
 						/>
 						<Typography variant='sm' className='m-0 mr-2 rounded border-solid border-inherit border-primary-500 bg-primary-100 p-0'>
 							%
