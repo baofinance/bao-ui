6dc412b161f7e788b9ad2adbfb2674b1d8b9ee46
Vitalsine85 <rob@creative-element.net>
2023-05-15 16:44:49 -0400

    supply modal fin

--- src/pages/vaults/components/Modals/SupplyModal.tsx
+++ src/pages/vaults/components/Modals/SupplyModal.tsx
@@ -23,7 +23,7 @@ export type SupplyModalProps = {
 }
 
 const SupplyModal = ({ asset, show, onHide, vaultName, val }: SupplyModalProps) => {
-	const { pendingTx, handleTx } = useTransactionHandler()
+	const { pendingTx, txSuccess, handleTx } = useTransactionHandler()
 	const { approvals } = useApprovals(vaultName)
 	const { vaultContract } = asset
 	const erc20 = useContract<Erc20>('Erc20', asset.underlyingAddress)
@@ -34,70 +34,69 @@ const SupplyModal = ({ asset, show, onHide, vaultName, val }: SupplyModalProps) 
 	}, [onHide])
 
 	return (
-		<>
-			<Modal isOpen={show} onDismiss={hideModal}>
-				<Modal.Header onClose={hideModal}>
-					<div className='mx-0 my-auto flex h-full items-center text-baoWhite'>
-						<Typography variant='xl' className='mr-1 inline-block'>
-							Confirm Deposit
+		<Modal isOpen={show} onDismiss={hideModal}>
+			<Modal.Header onClose={hideModal}>
+				<div className='mx-0 my-auto flex h-full items-center text-baoWhite'>
+					<Typography variant='xl' className='mr-1 inline-block'>
+						Confirm Deposit
+					</Typography>
+				</div>
+			</Modal.Header>
+			<Modal.Body>
+				{!pendingTx ? (
+					<Typography variant='lg' className='p-6 text-center font-bakbak'>
+						<Image src={`/images/tokens/${asset.icon}`} width={32} height={32} alt={asset.underlyingSymbol} className='inline p-1' />
+						{getDisplayBalance(val, asset.underlyingDecimals).toString()} {asset.underlyingSymbol}{' '}
+						<Badge>${getDisplayBalance(decimate(usdValue))}</Badge>
+					</Typography>
+				) : (
+					<div className='flex flex-row justify-center p-6 align-middle'>
+						<MoonLoader size={24} speedMultiplier={0.8} color='#e21a53' className='align-middle' />
+						<Typography variant='lg' className='ml-4 mt-1 inline-block align-middle font-bakbak'>
+							Pending Transaction
 						</Typography>
 					</div>
-				</Modal.Header>
-				<>
-					<Modal.Body>
-						<Typography variant='lg' className='p-6 text-center font-bakbak'>
-							<Image src={`/images/tokens/${asset.icon}`} width={32} height={32} alt={asset.underlyingSymbol} className='inline p-1' />
-							{getDisplayBalance(val, asset.underlyingDecimals).toString()} {asset.underlyingSymbol}{' '}
-							<Badge>${getDisplayBalance(decimate(usdValue))}</Badge>
-						</Typography>
-					</Modal.Body>
-					<Modal.Actions>
-						{!pendingTx ? (
-							approvals && (asset.underlyingAddress === 'ETH' || approvals[asset.underlyingAddress].gt(0)) ? (
-								<Button
-									fullWidth
-									className='!rounded-full'
-									disabled={!val}
-									onClick={async () => {
-										let supplyTx
-										if (asset.underlyingAddress === 'ETH') {
-											// @ts-ignore
-											supplyTx = vaultContract.mint(true, {
-												value: val,
-											})
-											// TODO- Give the user the option in the SupplyModal to tick collateral on/off
-										} else {
-											// @ts-ignore
-											supplyTx = vaultContract.mint(val, true) // TODO- Give the user the option in the SupplyModal to tick collateral on/off
-										}
-										handleTx(supplyTx, `${vaultName} Vault: Supply ${val} ${asset.underlyingSymbol}`, () => onHide())
-									}}
-								>
-									Confirm Deposit
-								</Button>
-							) : (
-								<Button
-									fullWidth
-									className='!rounded-full'
-									disabled={!approvals}
-									onClick={() => {
-										// TODO- give the user a notice that we're approving max uint and instruct them how to change this value.
-										const tx = erc20.approve(vaultContract.address, ethers.constants.MaxUint256)
-										handleTx(tx, `${vaultName} Vault: Approve ${asset.underlyingSymbol}`)
-									}}
-								>
-									Approve
-								</Button>
-							)
-						) : (
-							<Button fullWidth disabled={true}>
-								<MoonLoader size={12} speedMultiplier={0.8} color='#e21a53' /> Pending Transaction
-							</Button>
-						)}
-					</Modal.Actions>
-				</>
-			</Modal>
-		</>
+				)}
+			</Modal.Body>
+			<Modal.Actions>
+				{approvals && (asset.underlyingAddress === 'ETH' || approvals[asset.underlyingAddress].gt(0)) ? (
+					<Button
+						fullWidth
+						className='!rounded-full'
+						disabled={!val || pendingTx}
+						onClick={async () => {
+							let supplyTx
+							if (asset.underlyingAddress === 'ETH') {
+								// @ts-ignore
+								supplyTx = vaultContract.mint(true, {
+									value: val,
+								})
+								// TODO- Give the user the option in the SupplyModal to tick collateral on/off
+							} else {
+								// @ts-ignore
+								supplyTx = vaultContract.mint(val, true) // TODO- Give the user the option in the SupplyModal to tick collateral on/off
+							}
+							handleTx(supplyTx, `${vaultName} Vault: Supply ${val} ${asset.underlyingSymbol}`, () => onHide())
+						}}
+					>
+						Confirm
+					</Button>
+				) : (
+					<Button
+						fullWidth
+						className='!rounded-full'
+						disabled={!approvals}
+						onClick={() => {
+							// TODO- give the user a notice that we're approving max uint and instruct them how to change this value.
+							const tx = erc20.approve(vaultContract.address, ethers.constants.MaxUint256)
+							handleTx(tx, `${vaultName} Vault: Approve ${asset.underlyingSymbol}`)
+						}}
+					>
+						Approve
+					</Button>
+				)}
+			</Modal.Actions>
+		</Modal>
 	)
 }
 
