246905c3af5c8633bfaac7fc7d182a93e9e5ba45
Vitalsine85 <rob@creative-element.net>
2023-05-17 12:05:00 -0400

    footer padding

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import Container from '../Container'
 import Nav from './components/Nav'
 
 const Footer: React.FC = () => (
-	<footer className='bottom-0 left-0 m-auto mb-8 w-full p-4'>
+	<footer className='bottom-0 left-0 m-auto mb-8 w-full p-16'>
 		<div className='flex flex-1 justify-center'>
 			<Nav />
 		</div>
