7932182f597da0916a06f4a027a4928ef2c7273e
Vitalsine85 <rob@creative-element.net>
2023-07-12 00:20:59 -0400

    update walletconnect

--- src/bao/lib/connectors.ts
+++ src/bao/lib/connectors.ts
@@ -1,6 +1,6 @@
+import { WalletConnectV2Connector } from '@/utils/walletconnectV2Connector'
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { NetworkConnector } from '@web3-react/network-connector'
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 
 const supportedChainIds = [1]
@@ -31,8 +31,34 @@ export const injected = new InjectedConnector({
 	supportedChainIds,
 })
 
-export const walletConnect = new WalletConnectConnector({
-	rpc: RPC_URLS,
+export const walletConnect = new WalletConnectV2Connector({
+	projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECTID,
+	rpcMap: RPC_URLS,
+	chains: [1],
+	showQrModal: true,
+	// Decentraland's RPCs don't support the `test` method used for the ping.
+	disableProviderPing: true,
+	qrModalOptions: {
+		themeVariables: {
+			// Display the WC modal over other Decentraland UI's modals.
+			// Won't be visible without this.
+			'--wcm-z-index': '9999',
+		},
+	},
+	// Methods and events based on what is used on the decentraland dapps and the ethereum-provider lib found at:
+	// https://github.com/WalletConnect/walletconnect-monorepo/blob/v2.0/providers/ethereum-provider/src/constants/rpc.ts
+	// If the wallet doesn't support non optional methods, it will not allow the connection.
+	methods: ['eth_sendTransaction', 'personal_sign'],
+	optionalMethods: [
+		'eth_accounts',
+		'eth_requestAccounts',
+		'eth_sign',
+		'eth_signTypedData_v4',
+		'wallet_switchEthereumChain',
+		'wallet_addEthereumChain',
+	],
+	events: ['chainChanged', 'accountsChanged'],
+	optionalEvents: ['disconnect'],
 })
 
 export const coinbaseWallet = new WalletLinkConnector({
